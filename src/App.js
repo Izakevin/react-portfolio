2d3629cfd7ea567b651bc412a610c0946598d97d
Izakevin <keviniza64@gmail.com>
2022-08-16 21:16:14 -0400

    Added in the header information and styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
       <header>
-        <h1>Kevin Iza</h1>
-        <nav id='Navagation'>
-          <ul>About Me</ul>
-          <ul>Skills</ul>
-          <ul>Socials</ul>
-        </nav>
+        <div id="hr-top">
+          <p>Kevin Iza Software Developer</p>
+
+          <nav id='Navagation'>
+            <ul>About Me</ul>
+            <ul>Education</ul>
+            <ul>Projects</ul>
+            <ul>Skills</ul>
+            <ul>Contact</ul>
+          </nav>
+        </div>
+
+        <div id="hr-info">
+          <h1>Hello, World! Im Kevin</h1>
+          <p>I am a software developer searching for a new spawn point and grinding levels to upgrade my skill tree</p>
+
+          <div id="hr-buttons">
+            <button onClick={""}>Projects</button>
+            <button onClick={""}>Github</button>
+          </div>
+        </div>
+
       </header>
 
       <body>
