258aa0a3ea53f004fdbfb191e3e2d7b6d29b262c
Izakevin <keviniza64@gmail.com>
2022-08-23 23:29:51 -0400

    Created Education layout and fixed Education image display bug

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,7 +6,7 @@ function Education(){
             <h2>Education</h2>
 
             <div id = "Edu_layout">
-                <img src='./Logos/tandon_seal.webp' alt="New York University Tandon School Seal"/>
+                <img src={require('./Logos/tandon_seal.webp')} alt="New York University Tandon School Seal"/>
                 <p>A bachelor's degree of science from New York University Tandon School of Engineering
             Minor in Studies of Technology, Science, and Society
                 </p>
