0ada6f58efe490806aae30d63b4c44180e551637
Izakevin <keviniza64@gmail.com>
2022-08-26 19:59:03 -0400

    Made About Me section into a component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import Header from './components/Header.js';
 import Education from './components/Education.js';
 import Skills from './components/Skills.js';
 import Contact from './components/Contact';
+import AboutMe from './components/About_Me';
 
 function App() {
-
-  // const aboutmeRef = useRef(null);
 
   return (
     <div>
 
       <Header/>
       <body>
-        <div id="about_me">
-          <h2 className='section'>About Me</h2>
-          <p>I am Kevin Iza a software developer from Brooklyn New York. My interest in technology started when I realized I had witnessed a shift in the portable phone market from flip and sliding phones to smartphones with touch displays. The rapid change and adoption were fascinating to see and look back on.
-          </p>
-        </div>
+
+        <AboutMe/>
 
         <Education/>
 
