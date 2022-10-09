42be709f71057fc63e4c2ce6cc26e13f9361b746
Izakevin <keviniza64@gmail.com>
2022-10-09 14:06:04 -0400

    updated Education Text layout

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,11 +5,15 @@ function Education(){
         <div id="education">
             <h2 className='section'>Education</h2>
 
-            <div id = "Edu_layout">
+            <div id = "edu_layout">
                 <img src={require('./Logos/tandon_seal.webp')} alt="New York University Tandon School Seal"/>
-                <p>A bachelor's degree of science from New York University Tandon School of Engineering
-            Minor in Studies of Technology, Science, and Society
-                </p>
+                <div id = "edu_txt_layout">
+                    <h3>New York University Tandon School of Engineering</h3>
+
+                    <p>Title: Bachelors Degree of Science in Computer Science</p>
+
+                    <p>Minor: Studies of Technology, Science, and Society</p>
+                </div>
             </div> 
 
         </div>
